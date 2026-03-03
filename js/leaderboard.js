99f24690ee0358fcd2bd908f2a7d5ec9e7c9714e
gilangsite <berceritague@gmail.com>
2026-03-03 14:36:29 +0700

    Fix: Update APPS_SCRIPT_URL to new deployment ID

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 10000);
 });
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyVM4EKlT1iSUUj7970-YdYAynbqpUaBAcTSMlJ_H_4Th9tSB4D0vCPscMzlb5BRihIBQ/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby4K1ZDyEHiU3Jm5gG91E32_tFD4m3nHc68AilD9-M7FxrK4PVt_YwPp-lDl5iOAjI_/exec';
 let currentUserEmail = localStorage.getItem('mercy_quiz_email') || '';
 let isGenerating = false;
 
